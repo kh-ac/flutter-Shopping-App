12aa482403ceab8dee9d27395f18378c6778bf4c
Khalid-AC <khalid.aici.ac@gmail.com>
2022-07-29 16:19:43 +0100

    Shopping Lists Screen

---
+++ lib/routes/shopping_lists_screen.dart
@@ -0,0 +1,86 @@
+import "package:flutter/material.dart";
+import 'package:flutter_sqlite/models/shopping_list.dart';
+import 'package:flutter_sqlite/utils/db_helper.dart';
+import 'package:flutter_sqlite/widgets/shopping_list_dialog.dart';
+
+class ShoppingListsScreen extends StatefulWidget {
+  ShoppingListsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ShoppingListsScreen> createState() => _ShoppingListsScreenState();
+}
+
+class _ShoppingListsScreenState extends State<ShoppingListsScreen> {
+  //
+  List<ShoppingList> shoppingLists = [];
+
+  //
+  DbHelper dbHelper = DbHelper();
+
+  //
+  Future<void> getData() async {
+    await dbHelper.openDb();
+    shoppingLists = await dbHelper.getLists();
+    setState(() {
+      shoppingLists = shoppingLists;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getData();
+    return Scaffold(
+      //
+      appBar: AppBar(
+        title: Text("Shopping Lists"),
+      ),
+
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          //
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                //
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+
+                //
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                elevation: 8,
+
+                //
+                child: ListTile(
+                  //
+                  leading: CircleAvatar(
+                      child: Text(shoppingLists[index].priority.toString())),
+
+                  //
+                  title: Text(shoppingLists[index].name),
+
+                  //
+                  trailing: const Icon(Icons.edit),
+                ),
+              );
+            },
+            itemCount: shoppingLists.length,
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return ShoppingListDialog(
+                    shoppingList: ShoppingList(id: 0, name: "", priority: 0),
+                    isNew: true);
+              });
+        },
+      ),
+    );
+  }
+}
