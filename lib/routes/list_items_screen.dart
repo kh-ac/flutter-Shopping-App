b4c318217fe5085de15e33e78283c620aa34bbda
Khalid-AC <khalid.aici.ac@gmail.com>
2022-07-29 16:19:11 +0100

    List items Screen

---
+++ lib/routes/list_items_screen.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class ListItemsScreen extends StatefulWidget {
+  ListItemsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListItemsScreen> createState() => _ListItemsScreenState();
+}
+
+class _ListItemsScreenState extends State<ListItemsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
