ce1103134f7d915c198170d30b91f14725b9499c
Khalid-AC <khalid.aici.ac@gmail.com>
2022-07-30 01:08:25 +0100

    toMap Added

--- lib/models/list_item.dart
+++ lib/models/list_item.dart
@@ -1,5 +1,24 @@
+class ListItem {
+  int id;
+  int idList;
+  String name;
+  String quantity;
+  String note;
 
+  ListItem(
+      {required this.id,
+      required this.idList,
+      required this.name,
+      required this.quantity,
+      required this.note });
 
-class ListItem{
-  
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id == 0 ? null : id,
+      "idList": idList,
+      "name": name,
+      "quantity": quantity,
+      "note": note,
+    };
+  }
 }
